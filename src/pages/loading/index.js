2330fd56197169f09de7e85dfb67ed46a9daaf83
chinmaisiddhartha <chinmai.siddhartha@gmail.com>
2023-04-15 05:30:25 +0530

    initial commit

---
+++ src/pages/loading/index.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import '../loading/style.css';
+import { Redirect } from 'react-router-dom';
+import ReactGA from 'react-ga';
+import {Helmet} from 'react-helmet';
+import PuffLoader from "react-spinners/PuffLoader";
+
+class Loading extends Component {
+
+  state = {
+    redirect: false
+  }
+
+  componentDidMount() {
+     this.id = setTimeout(() => this.setState({ redirect: true }), 3000);
+    //--Chinmai Siddharthas Portfolio website Google analytics--
+    //--Replace with your own key !!
+    // ReactGA.initialize("UA-154721739-1");
+    // ReactGA.pageview('motasimfoad.com - Loading Screen');
+    //--Chinmai Siddharthas Portfolio website Google analytics--
+  }
+    
+  componentWillUnmount() {
+    clearTimeout(this.id)
+  }
+
+  render() {
+    return this.state.redirect
+      ? <Redirect to="/home" />
+      : <div>
+         <Helmet>
+                <meta charSet="utf-8" />
+                <title>Chinmai Siddhartha</title>
+                <link rel="canonical" href="http://ncsid.dev/" />
+                <meta name="description" content="Chinmai Siddhartha - Product Manager | Project Manager | Software Engineer" />
+         </Helmet>
+        <div className="Loading-header">
+          <PuffLoader
+            size={'40vw'}
+            color={"#fff"}
+            loading={true}
+          />
+        </div>
+        </div>
+  }
+}
+
+export default Loading;
