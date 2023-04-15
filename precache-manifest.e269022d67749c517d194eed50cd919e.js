b5b0ae482095b993bc85dee3d58d62e187344eaa
chinmaisiddhartha <chinmai.siddhartha@gmail.com>
2023-04-15 06:08:17 +0530

    Updates

---
+++ precache-manifest.e269022d67749c517d194eed50cd919e.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c74bfd611c01c965414cb1024e1246c",
+    "url": "/sidportfolio/index.html"
+  },
+  {
+    "revision": "5a56eb834aff90adf984",
+    "url": "/sidportfolio/static/css/2.d2ca2446.chunk.css"
+  },
+  {
+    "revision": "54171aae348310b0039a",
+    "url": "/sidportfolio/static/css/main.b428e104.chunk.css"
+  },
+  {
+    "revision": "5a56eb834aff90adf984",
+    "url": "/sidportfolio/static/js/2.d6f35684.chunk.js"
+  },
+  {
+    "revision": "54171aae348310b0039a",
+    "url": "/sidportfolio/static/js/main.2c9151cd.chunk.js"
+  },
+  {
+    "revision": "9aa548c1e6023db8f9ec",
+    "url": "/sidportfolio/static/js/runtime-main.171c9d42.js"
+  },
+  {
+    "revision": "5c653daace009c6d0d9c4fd8f7f265ff",
+    "url": "/sidportfolio/static/media/cv.5c653daa.png"
+  },
+  {
+    "revision": "54c8b12a4400ad41b9c408c170cc2070",
+    "url": "/sidportfolio/static/media/cv1.54c8b12a.png"
+  },
+  {
+    "revision": "4c1cba5f7496cf94240aa8aa3d67f76a",
+    "url": "/sidportfolio/static/media/cv2.4c1cba5f.png"
+  },
+  {
+    "revision": "2da239b51f31c877b33bb3bf4e81c849",
+    "url": "/sidportfolio/static/media/cv3.2da239b5.png"
+  },
+  {
+    "revision": "fdf35f0af93d30d092cb284563dbb732",
+    "url": "/sidportfolio/static/media/cv4.fdf35f0a.png"
+  },
+  {
+    "revision": "8f34d1233bf62b4c833d117044fef289",
+    "url": "/sidportfolio/static/media/foad35.8f34d123.png"
+  },
+  {
+    "revision": "0a04cd22a69442e31c3df8f8e1adff1b",
+    "url": "/sidportfolio/static/media/foad_logo.0a04cd22.jpg"
+  },
+  {
+    "revision": "c31b5069f157b82943809ab2ecc38ef8",
+    "url": "/sidportfolio/static/media/git.c31b5069.svg"
+  }
+]);
